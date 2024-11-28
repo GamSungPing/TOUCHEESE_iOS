b4307d5ee5f4ff6f194a750cab2519785980b1aa
Kimseongmin <sjybext@naver.com>
2024-11-28 21:34:49 +0900

    feat: 가격, 리뷰를 선택하기 위한 CustomSegmentedControl 구현
    - Shape 프로토콜을 따르며, 원하는 모서리만 Round 처리할 수 있는 Shape 구현
    - 그 Shape을 이용해서 CustomSegmentedControl 구현
    - Studio 설명 Label에 리뷰 개수 추가

--- TOUCHEESE/TOUCHEESE/Views/Home/StudioDetailView.swift
+++ TOUCHEESE/TOUCHEESE/Views/Home/StudioDetailView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct StudioDetailView: View {
     @StateObject var viewModel: StudioDetailViewModel
     
+    @State private var selectedSegmentedControlIndex = 0
+    
     var body: some View {
         let studio = viewModel.studio
         let studioDetail = viewModel.studioDetail
@@ -23,11 +25,28 @@ struct StudioDetailView: View {
                 
                 // Studio 설명
                 VStack(alignment: .leading, spacing: 6) {
-                    Label("\(studio.formattedRating)", systemImage: "star")
-                    Label("\(studioDetail.businessHours)", systemImage: "clock")
-                    Label("\(studioDetail.address)", systemImage: "mappin.and.ellipse")
+                    Label(
+                        "\(studio.formattedRating) (리뷰 \(studioDetail.reviewCount)개)",
+                        systemImage: "star"
+                    )
+                    Label(
+                        "\(studioDetail.businessHours)",
+                        systemImage: "clock"
+                    )
+                    Label(
+                        "\(studioDetail.address)",
+                        systemImage: "mappin.and.ellipse"
+                    )
                 }
                 .padding(.horizontal)
+                
+                CustomSegmentedControl(selectedIndex: $selectedSegmentedControlIndex)
+                
+                if selectedSegmentedControlIndex == 0 {
+                    productListView
+                } else {
+                    reviewGridView
+                }
             }
         }
         .toolbarRole(.editor)
@@ -71,7 +90,60 @@ struct StudioDetailView: View {
             }
         }
     }
+    
+    private var productListView: some View {
+        Text("상품 리스트 표시")
+    }
+    
+    private var reviewGridView: some View {
+        Text("리뷰 그리드 표시")
+    }
 }
+
+
+fileprivate struct CustomSegmentedControl: View {
+    @Binding var selectedIndex: Int
+    let options = ["가격", "리뷰"]
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(options.indices, id:\.self) { index in
+                ZStack {
+                    RoundedCornersShape(corners: [.topLeft, .topRight], radius: 20)
+                        .fill(.tcLightyellow)
+                    
+                    RoundedCornersShape(corners: [.topLeft, .topRight], radius: 20)
+                        .fill(.tcYellow)
+                        .opacity(selectedIndex == index ? 1 : 0.01)
+                        .onTapGesture {
+                            selectedIndex = index
+                        }
+                }
+                .overlay {
+                    Text(options[index])
+                }
+            }
+        }
+        .frame(height: 40)
+    }
+}
+
+
+fileprivate struct RoundedCornersShape: Shape {
+    var corners: UIRectCorner
+    var radius: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        
+        return Path(path.cgPath)
+    }
+}
+
 
 #Preview {
     NavigationStack {
