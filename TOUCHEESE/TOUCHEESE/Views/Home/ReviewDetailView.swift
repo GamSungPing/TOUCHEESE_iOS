ebba3fb538d1bfe38f5fb62b38fafbd2491a9531
Healthy <kangsworkspace@gmail.com>
2024-12-02 21:26:53 +0900

    design: ReviewDetailView 뷰 분리

--- TOUCHEESE/TOUCHEESE/Views/Home/ReviewDetailView.swift
+++ TOUCHEESE/TOUCHEESE/Views/Home/ReviewDetailView.swift
@@ -24,9 +24,10 @@ struct ReviewDetailView: View {
     
     var body: some View {
         ScrollView(Axis.Set.vertical, showsIndicators: false) {
-            VStack(alignment: .leading) {
+            VStack {
                 DividerView(horizontalPadding: 0, color: .tcLightyellow)
                 
+                // 스튜디오 이미지 캐러셸 뷰
                 ImageCarouselView(
                     imageURLs: reviewDetail.imageURLs,
                     carouselIndex: $carouselIndex,
@@ -35,57 +36,14 @@ struct ReviewDetailView: View {
                 )
                 .padding(.bottom, 15)
                 
-                Group {
-                    Text("\(reviewDetail.content)")
-                        .multilineTextAlignment(.leading)
-                        .padding(.bottom, 10)
-                    
-                    HStack {
-                        Image(systemName: "star.fill")
-                            .foregroundStyle(.yellow)
-                        
-                        Text(String(format: "%.1f", reviewDetail.rating))
-                        
-                        Spacer()
-                        
-                        Text("\(reviewDetail.dateString.iso8601ToDate?.toString(format: .yeatMonthDay) ?? "")")
-                            .foregroundStyle(.gray)
-                            .font(.footnote)
-                            .padding(.trailing)
-                    }
-                }
-                .padding(.leading, 15)
+                // 사용자가 작성한 리뷰 뷰
+                reviewContentView(reviewDetail: reviewDetail)
                 
                 DividerView(horizontalPadding: 10)
                     .padding(.bottom)
                 
-                Group {
-                    HStack(alignment: .top) {
-                        ProfileImageView(imageURL: studioProfileImageString, size: 40)
-                        
-                        VStack(alignment: .leading) {
-                            HStack {
-                                Text("\(reply.studioName)")
-                                    .fontWeight(.bold)
-                                
-                                Text("\(day ?? "알 수 없음")")
-                                    .foregroundStyle(.gray)
-                                    .font(.footnote)
-                                
-                                Spacer()
-                                
-                            }
-                            .padding(.top, 4)
-                            
-                            Text("\(reply.content)")
-                                .font(.caption)
-                         
-                            Spacer()
-                        }
-                        .padding(.horizontal)
-                    }
-                }
-                .padding(.leading, 15)
+                // 사용자가 작성한 리뷰에 대한 스튜디오의 댓글 뷰
+                reviewReplyView(reply: reply, studioProfileImageString: studioProfileImageString)
                 
                 Spacer()
             }
@@ -101,6 +59,57 @@ struct ReviewDetailView: View {
                 isShowingImageExtensionView: $isShowingImageExtensionView
             )
         }
+    }
+    
+    private func reviewContentView(reviewDetail: ReviewDetail) -> some View {
+        VStack(alignment: .leading) {
+            Text("\(reviewDetail.content)")
+                .multilineTextAlignment(.leading)
+                .padding(.bottom, 10)
+            
+            HStack {
+                Image(systemName: "star.fill")
+                    .foregroundStyle(.yellow)
+                
+                Text(String(format: "%.1f", reviewDetail.rating))
+                
+                Spacer()
+                
+                Text("\(reviewDetail.dateString.iso8601ToDate?.toString(format: .yeatMonthDay) ?? "")")
+                    .foregroundStyle(.gray)
+                    .font(.footnote)
+                    .padding(.trailing)
+            }
+        }
+        .padding(.leading)
+    }
+    
+    private func reviewReplyView(reply: Reply, studioProfileImageString: URL) -> some View {
+        HStack(alignment: .top) {
+            ProfileImageView(imageURL: studioProfileImageString, size: 40)
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("\(reply.studioName)")
+                        .fontWeight(.bold)
+                    
+                    Text("\(day ?? "알 수 없음")")
+                        .foregroundStyle(.gray)
+                        .font(.footnote)
+                    
+                    Spacer()
+                    
+                }
+                .padding(.top, 4)
+                
+                Text("\(reply.content)")
+                    .font(.caption)
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+        }
+        .padding(.leading)
     }
     
     private func leadingToolbarContent(
