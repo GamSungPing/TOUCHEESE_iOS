dc84e0e65ab06b0f396dc5bef4c522d301266566
Geon Kang <kangsworkspace@gmail.com>
2024-12-11 14:52:28 +0900

    feat: Pretendard Font 에셋, 확장자 추가 (#35)

---
+++ TOUCHEESE/TOUCHEESE/Service/Extensions/Font+UseCase.swift
@@ -0,0 +1,28 @@
+//
+//  Font+UseCase.swift
+//  TOUCHEESE
+//
+//  Created by Healthy on 12/11/24.
+//
+
+import Foundation
+import SwiftUICore
+
+extension Font {
+    // Bold
+    static let pretendardBold18: Font = .custom("Pretendard-Bold", size: 18)
+    static let pretendardBold20: Font = .custom("Pretendard-Bold", size: 20)
+    
+    // SemiBold
+    static let pretendardSemiBold16: Font = .custom("Pretendard-SemiBold", size: 16)
+    static let pretendardSemiBold18: Font = .custom("Pretendard-SemiBold", size: 18)
+    static let pretendardSemiBold22: Font = .custom("Pretendard-SemiBold", size: 22)
+
+    // Medium
+    static let pretendardMedium14: Font = .custom("Pretendard-Medium", size: 14)
+    static let pretendardMedium16: Font = .custom("Pretendard-Medium", size: 16)
+    
+    // Regular
+    static let pretendardRegular14: Font = .custom("Pretendard-Regular", size: 14)
+    static let pretendardRegular16: Font = .custom("Pretendard-Regular", size: 16)
+}
