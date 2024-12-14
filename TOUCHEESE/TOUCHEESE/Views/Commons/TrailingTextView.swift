afef7ad18553ebf8bfabadd911b3760f7d86dd0f
Healthy <kangsworkspace@gmail.com>
2024-12-14 16:24:52 +0900

    feat: ReservationConfirmView UI 작업

---
+++ TOUCHEESE/TOUCHEESE/Views/Commons/TrailingTextView.swift
@@ -0,0 +1,34 @@
+//
+//  TrailingTextView.swift
+//  TOUCHEESE
+//
+//  Created by Healthy on 12/13/24.
+//
+
+import SwiftUI
+
+struct TrailingTextView: View {
+    let text: String
+    let font: Font?
+    let textColor: Color?
+    
+    init(text: String, font: Font? = nil, textColor: Color? = nil) {
+        self.text = text
+        self.font = font
+        self.textColor = textColor
+    }
+    
+    var body: some View {
+        HStack {
+            Text(text)
+                .font(font ?? .pretendardBold18)
+                .foregroundStyle(textColor ?? .tcGray10)
+
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    TrailingTextView(text: "예약 정보")
+}
