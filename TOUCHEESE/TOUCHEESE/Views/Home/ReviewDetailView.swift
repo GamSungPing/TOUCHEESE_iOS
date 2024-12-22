8e380364500c918fa7a19500d077f9351a08afe6
Kimseongmin <sjybext@naver.com>
2024-12-22 14:31:19 +0900

    design: ReviewDetailView 디자인 반영

--- TOUCHEESE/TOUCHEESE/Views/Home/ReviewDetailView.swift
+++ TOUCHEESE/TOUCHEESE/Views/Home/ReviewDetailView.swift
@@ -34,9 +34,7 @@ struct ReviewDetailView: View {
                 
                 // 사용자가 작성한 리뷰 뷰
                 reviewContentView(reviewDetail: reviewDetail)
-                
-                DividerView(horizontalPadding: 10)
-                    .padding(.bottom)
+                    .padding(.horizontal, 16)
                 
                 // 사용자가 작성한 리뷰에 대한 스튜디오의 댓글 뷰
                 reviewReplyView(reply: reply, studio: studio)
@@ -67,65 +65,92 @@ struct ReviewDetailView: View {
     }
     
     private func reviewContentView(reviewDetail: ReviewDetail) -> some View {
-        VStack(alignment: .leading) {
-            Text("\(reviewDetail.content ?? "")")
-                .multilineTextAlignment(.leading)
-                .padding(.bottom, 10)
-            
-            HStack {
-                Label {
-                    Text(String(format: "%.1f", reviewDetail.rating))
-                        .foregroundColor(.primary)
-                } icon: {
-                    Image(systemName: "star.fill")
-                        .foregroundColor(.yellow)
+        VStack(alignment: .leading, spacing: 0) {
+            HStack(spacing: 8) {
+                ProfileImageView(
+                    imageURL: reviewDetail.userProfileImageURL,
+                    size: 46
+                )
+                
+                VStack(alignment: .leading, spacing: 3) {
+                    Text(reviewDetail.userName)
+                        .foregroundStyle(.tcGray08)
+                        .font(.pretendardMedium16)
+                    
+                    HStack {
+                        Image(.tcStarFill)
+                            .resizable()
+                            .frame(width: 14, height: 14)
+                        
+                        Text(String(format: "%.1f", reviewDetail.rating))
+                            .foregroundStyle(.tcGray08)
+                            .font(.pretendardMedium14)
+                    }
                 }
                 
-                Text("\(reviewDetail.dateString.iso8601ToDate?.toString(format: .yearMonthDay) ?? "")")
-                    .foregroundStyle(.gray)
-                    .font(.footnote)
-                    .padding(.leading, 5)
-                
                 Spacer()
+                
+                VStack {
+                    Text("작성일: \(reviewDetail.dateString.iso8601ToDate?.toString(format: .yearMonthDay) ?? "")")
+                        .foregroundStyle(.tcGray05)
+                        .font(.pretendardRegular12)
+                    
+                    Spacer()
+                }
             }
+            .padding(.horizontal, 16)
+            
+            Text("\(reviewDetail.content ?? "")")
+                .foregroundStyle(.tcGray08)
+                .font(.pretendardRegular14)
+                .multilineTextAlignment(.leading)
+                .padding(16)
         }
-        .padding(.leading)
     }
     
     private func reviewReplyView(
         reply: Reply?,
         studio: Studio
     ) -> some View {
-        VStack {
+        VStack(spacing: 20) {
             if let reply {
-                HStack(alignment: .top) {
-                    ProfileImageView(
-                        imageURL: studio.profileImageURL,
-                        size: 40
-                    )
-                    
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Text("\(reply.studioName)")
-                                .fontWeight(.bold)
-                            
-                            Text("\(reply.dateString.iso8601ToDate?.toString(format: .yearMonthDay) ?? "알 수 없음")")
-                                .foregroundStyle(.gray)
-                                .font(.footnote)
-                            
-                            Spacer()
-                            
-                        }
-                        .padding(.top, 4)
+                DividerView(height: 8)
+                
+                VStack(alignment: .leading, spacing: 12) {
+                    HStack(spacing: 4) {
+                        ProfileImageView(
+                            imageURL: studio.profileImageURL,
+                            size: 46
+                        )
                         
-                        Text("\(reply.content)")
-                            .font(.caption)
+                        VStack(alignment: .leading, spacing: 3) {
+                            Text(studio.name)
+                                .foregroundStyle(.tcGray10)
+                                .font(.pretendardMedium16)
+                            
+                            Text("작성일: \(reply.dateString.iso8601ToDate?.toString(format: .yearMonthDay) ?? "")")
+                                .foregroundStyle(.tcGray05)
+                                .font(.pretendardRegular12)
+                        }
                         
                         Spacer()
                     }
-                    .padding(.horizontal)
+                    
+                    Text(reply.content)
+                        .foregroundStyle(.tcGray07)
+                        .font(.pretendardRegular14)
+                        .multilineTextAlignment(.leading)
                 }
-                .padding(.leading)
+                .padding(16)
+                .background {
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(.tcGray01)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(.tcGray02, lineWidth: 1)
+                        }
+                }
+                .padding(.horizontal, 16)
             }
         }
     }
