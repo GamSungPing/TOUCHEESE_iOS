54133e9e2cd578879bd1148f38de3fcb784660ac
Kimseongmin <sjybext@naver.com>
2024-12-22 23:11:43 +0900

    temp: Apple Log In 테스트 구현

--- TOUCHEESE/TOUCHEESE/Views/TestView2.swift
+++ TOUCHEESE/TOUCHEESE/Views/TestView2.swift
@@ -6,13 +6,30 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
 struct TestView2: View {
     var body: some View {
-        VStack {
-            Spacer()
-            Text("Hello, World!")
-            Spacer()
+        SignInWithAppleButton { request in
+            request.requestedScopes = []
+        } onCompletion: { result in
+            switch result {
+            case .success(let authResults):
+                print("Apple Authorization successful.")
+                handleAuthorization(authResults)
+            case .failure(let error):
+                print("Apple Authorization failed: \(error.localizedDescription)")
+            }
+        }
+        .signInWithAppleButtonStyle(.black)
+        .frame(width: 20, height: 40)
+    }
+    
+    func handleAuthorization(_ authResults: ASAuthorization) {
+        if let appleIDCredential = authResults.credential as? ASAuthorizationAppleIDCredential {
+            let userIdentifier = appleIDCredential.user
+            
+            print("userIdentifier: \(userIdentifier)")
         }
     }
 }
